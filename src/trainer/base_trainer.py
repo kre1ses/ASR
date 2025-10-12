fixed freq masking

src/trainer/base_trainer.py
@@ -274,6 +274,7 @@ class BaseTrainer:
                 total=len(dataloader),
             ):
                 batch = self.process_batch(
+                    batch_idx,
                     batch,
                     metrics=self.evaluation_metrics,
                 )
@@ -549,7 +550,7 @@ class BaseTrainer:
             self.logger.info(f"Loading model weights from: {pretrained_path} ...")
         else:
             print(f"Loading model weights from: {pretrained_path} ...")
-        checkpoint = torch.load(pretrained_path, self.device)
+        checkpoint = torch.load(pretrained_path, self.device, weights_only=False)
 
         if checkpoint.get("state_dict") is not None:
             self.model.load_state_dict(checkpoint["state_dict"])

src/transforms/spec_augs/frequency_masking.py
@@ -1,18 +1,15 @@
-import random
-
 from torch import Tensor, nn
 from torchaudio import transforms as T
 
 
 class FrequencyMasking(nn.Module):
-    def __init__(self, p, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__()
-        self.p = p
         self._aug = T.FrequencyMasking(*args, **kwargs)
 
     def __call__(self, data: Tensor):
-        if random.random() < self.p:
-            x = data.unsqueeze(1)
-            return self._aug(x).squeeze(1)
-        else:
-            return data
+        x = data.unsqueeze(1)
+        x = x.transpose(0,1)
+        for i in range(4):
+            x = self._aug(x)
+        return x.squeeze(1)