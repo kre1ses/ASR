6bf245629e07392c6f8e7e305eab6f380f547683
Kirill <priborakirill276@gmail.com>
2025-10-19 02:21:59 +0300

    batch_norm

--- src/transforms/spec_augs/__init__.py
+++ src/transforms/spec_augs/__init__.py
@@ -1,7 +1,9 @@
 from src.transforms.spec_augs.frequency_masking import FrequencyMasking
 from src.transforms.spec_augs.time_masking import TimeMasking
+from src.transforms.spec_augs.normalize import Normalize1D
 
 __all__ = [
     "FrequencyMasking",
     "TimeMasking",
+    'Normalize1D',
 ]
