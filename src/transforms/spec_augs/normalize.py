a249e0cd848de438320be20cd7b83d3f432caaf1
Kirill <priborakirill276@gmail.com>
2025-10-19 02:04:02 +0300

    batch_norm

---
+++ src/transforms/spec_augs/normalize.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class Normalize1D(nn.Module):
+
+    def __init__(self, mean: float = None, std: float = None, eps: float = 1e-8):
+        super().__init__()
+        self.mean = mean
+        self.std = std
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        return (x - self.mean) / (self.std + self.eps)
